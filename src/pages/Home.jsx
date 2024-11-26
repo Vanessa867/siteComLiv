3ed0a4731694b273799cca48aefaf5c38138734c
Walber Pereira <116041394+Walberrr@users.noreply.github.com>
2024-11-26 17:58:19 +0000

    home e link

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,6 +8,7 @@ import livro from '../images/Books.png';
 import imagens1 from '../images/imgDois.2container.png';
 import imagens2 from '../images/imgTres.2container.png';
 import imagens3 from '../images/imgUm.2container.png'
+import imagens5 from '../images/imagem5.png'
 
 
 const Home = () => {
@@ -35,7 +36,7 @@ const Home = () => {
         </Box>
       </Container>
 
-       <Container maxWidth="lg" className="new-section">
+       <Container id="como-funciona" maxWidth="lg" className="new-section">
         <Typography variant="h3" className="new-section-title" gutterBottom>
          Participando de um clube
         </Typography>
@@ -67,6 +68,83 @@ const Home = () => {
           </Box>
         </Box>
       </Container>
+
+      <Container className="benefits-section" maxWidth="md">
+      <Typography variant="h4" className="section-title" gutterBottom>
+        Benefícios da Leitura
+      </Typography>
+      <Box className="benefits-list">
+        <Box className="benefit-item">
+          <Typography variant="h3" className="benefit-number">
+            1
+          </Typography>
+          <Box className="benefit-content">
+            <Typography variant="h6" className="benefit-title">
+              Desenvolve o Conhecimento e a Criatividade
+            </Typography>
+            <Typography variant="body2" className="benefit-description">
+              A leitura é uma porta de entrada para o conhecimento, oferecendo
+              insights sobre temas diversos e ampliando a compreensão do mundo.
+            </Typography>
+          </Box>
+        </Box>
+
+      <Box className="benefit-item">
+        <Typography variant="h3" className="benefit-number">
+          2
+        </Typography>
+        <Box className="benefit-content">
+          <Typography variant="h6" className="benefit-title">
+            Aprimora a Empatia e a Inteligência Emocional
+          </Typography>
+          <Typography variant="body2" className="benefit-description">
+            Ao vivenciar diferentes histórias e perspectivas, os leitores podem
+            entender melhor os dilemas e os conflitos alheios, fortalecendo sua
+            empatia.
+          </Typography>
+        </Box>
+      </Box>
+
+      <Box className="benefit-item">
+        <Typography variant="h3" className="benefit-number">
+          3
+        </Typography>
+        <Box className="benefit-content">
+          <Typography variant="h6" className="benefit-title">
+            Reduz o Estresse e Melhora o Bem-Estar Mental
+          </Typography>
+          <Typography variant="body2" className="benefit-description">
+            A leitura é uma forma eficaz de relaxamento, ajudando a aliviar o
+            estresse e promovendo uma sensação de calma e tranquilidade.
+          </Typography>
+        </Box>
+      </Box>
+    </Box>
+  </Container>
+
+  <Container className="community-section" maxWidth="lg">
+  <Box className="community-content">
+    <Box className="community-image">
+      <img src={imagens5} alt="Leitura em comunidade" className="image"
+      />
+    </Box>
+
+    <Box className="community-text">
+      <Typography variant="h4" className="community-title" gutterBottom>
+        Leitura em comunidade
+      </Typography>
+      <Typography variant="body1" className="community-description">
+        Ler em comunidade ou em clubes permite que os leitores compartilhem suas
+        impressões e entendimentos sobre um livro, o que enriquece a experiência
+        de leitura. Cada pessoa traz vivências e pontos de vista únicos, o que
+        abre portas para interpretações diversas e novas reflexões sobre a obra,
+        criando um aprendizado coletivo e único. Essa troca torna a leitura mais
+        dinâmica e reveladora, proporcionando uma compreensão mais rica do livro
+        e dos temas abordados.
+      </Typography>
+    </Box>
+  </Box>
+</Container>
 
       <footer>
         <Typography variant="body2" align="center" color="textSecondary">
